96e5d5a2ba52b244ea95d912d0b34ce72b916858
Michelle Kang <kang.mic@husky.neu.edu>
2021-02-26 19:06:37 -0500

    fixed login and set up store

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -13,12 +13,11 @@
           <md-input v-model="password"></md-input>
           <span class="md-helper-text">8 character minimum</span>
         </md-field>
-        <md-button class="md-raised" v-on:click="login()">Log in</md-button>
+        <md-button class="md-raised" @click="login(); $router.push('profile')">Login</md-button>
       </md-card-content>
     </md-card>
   </div>
 </template>
-
 <script>
 import axios from 'axios';
 
@@ -28,14 +27,25 @@ export default {
   data: () => ({
       email: '',
       password: '',
+      token: null,
   }),
+  computed: {
+    routing() {
+      if (this.token) {
+        return '/profile'
+      } else {
+        return ''
+      }
+    },
+  },
   methods: {
       async login() {
           const config = {headers: {'Content-Type': 'application/json'}}
           const formData = {'email': this.email, 'password': this.password};
           const response = await axios.post('http://localhost:4000/auth/login', formData, config);
-          console.log(response.data);
-      }
+          this.token = response.data;
+          console.log(this.token);
+      },
   }
 };
 </script>
