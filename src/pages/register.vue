a214678190b986074ddf3cf318cd34b8babc4bf4
michelle <michellekang621@gmail.com>
2022-01-28 15:41:11 -0800

    fixed bug

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -59,16 +59,7 @@ export default {
       const baseUrl = 'https://acnh-tool-backend.herokuapp.com'
       await axios.post(`${baseUrl}/auth/register`, formData, config)
       .then(() => {
-        let formData = {'email': this.email, 'password': this.password};
-        await axios.post(`${baseUrl}/auth/login`, formData, config)
-        .then((response) => {
-          this.token = response.data;
-          this.setUserByToken(this.token.token);
-          this.$router.push('profile');
-        })
-        .catch(function(error) {
-          return error;
-        });
+        this.$router.push({name: 'login'})
       });
     },
   },
